6e5a74f706df6aad9ac56296afa952cae69466fb
delaval <delaval.htps@gmail.com>
2021-01-25 16:20:59 +0100

    installation for checkstyle
    add maven-checkstyle-plugin
    add googleformatter-maven-plugin
    change of sources to respect checkstyle's google format

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,7 +1,9 @@
 package com.parkit.parkingsystem.integration;
 
-import com.parkit.parkingsystem.dao.ParkingSpotDAO;
-import com.parkit.parkingsystem.dao.TicketDAO;
+import static org.mockito.Mockito.when;
+
+import com.parkit.parkingsystem.dao.ParkingSpotDao;
+import com.parkit.parkingsystem.dao.TicketDao;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.service.ParkingService;
@@ -13,53 +15,50 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.mockito.Mockito.when;
+
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
+  private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+  private static ParkingSpotDao parkingSpotDAO;
+  private static TicketDao ticketDAO;
+  private static DataBasePrepareService dataBasePrepareService;
 
-    private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
-    private static ParkingSpotDAO parkingSpotDAO;
-    private static TicketDAO ticketDAO;
-    private static DataBasePrepareService dataBasePrepareService;
+  @Mock
+  private static InputReaderUtil inputReaderUtil;
 
-    @Mock
-    private static InputReaderUtil inputReaderUtil;
+  @BeforeAll
+  private static void setUp() throws Exception {
+    parkingSpotDAO = new ParkingSpotDao();
+    parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
+    ticketDAO = new TicketDao();
+    ticketDAO.dataBaseConfig = dataBaseTestConfig;
+    dataBasePrepareService = new DataBasePrepareService();
+  }
 
-    @BeforeAll
-    private static void setUp() throws Exception{
-        parkingSpotDAO = new ParkingSpotDAO();
-        parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
-        ticketDAO = new TicketDAO();
-        ticketDAO.dataBaseConfig = dataBaseTestConfig;
-        dataBasePrepareService = new DataBasePrepareService();
-    }
+  @BeforeEach
+  private void setUpPerTest() throws Exception {
+    when(inputReaderUtil.readSelection()).thenReturn(1);
+    when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+    dataBasePrepareService.clearDataBaseEntries();
+  }
 
-    @BeforeEach
-    private void setUpPerTest() throws Exception {
-        when(inputReaderUtil.readSelection()).thenReturn(1);
-        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
-        dataBasePrepareService.clearDataBaseEntries();
-    }
+  @AfterAll
+  private static void tearDown() {}
 
-    @AfterAll
-    private static void tearDown(){
+  @Test
+  public void testParkingACar() {
+    ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+    parkingService.processIncomingVehicle();
+    // TODO: check that a ticket is actualy saved in DB and Parking table is updated with
+    // availability
+  }
 
-    }
-
-    @Test
-    public void testParkingACar(){
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
-    }
-
-    @Test
-    public void testParkingLotExit(){
-        testParkingACar();
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
-    }
-
+  @Test
+  public void testParkingLotExit() {
+    testParkingACar();
+    ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+    parkingService.processExitingVehicle();
+    // TODO: check that the fare generated and out time are populated correctly in the database
+  }
 }
