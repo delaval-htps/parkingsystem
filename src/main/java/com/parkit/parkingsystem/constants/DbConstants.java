2e3f9575389a119caa4e20f56465ea0f94eb0311
delaval <delaval.htps@gmail.com>
2021-02-02 17:20:11 +0100

    update DbConstants.getTicket()
    resolution of the problem
    (see description in DbConstants)

--- src/main/java/com/parkit/parkingsystem/constants/DbConstants.java
+++ src/main/java/com/parkit/parkingsystem/constants/DbConstants.java
@@ -12,11 +12,21 @@ public class DbConstants {
           + "VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME) values(?,?,?,?,?)";
 
   public static final String UPDATE_TICKET = "update ticket set PRICE=?, OUT_TIME=? where ID=?";
+
+
+  // detected problem: when a vehicle exited ,getTicket() return the ticket of the first park use.
+  // So when user is a recurring , the far is calculated and store in this ticket of first park use
+  // with an update of its out_time( that is not null)
+  // Problem: the user has a bad fare with 5% of discount (but bad, calculated with his first
+  // in_time of park use) and when the vehicle is exited , we have got the good ticket in the DB
+  // with no price and no out_time.
+  // Resolution: select with the out_time = null
   public static final String GET_TICKET =
       "select t.PARKING_NUMBER, t.ID, t.PRICE, t.IN_TIME, t.OUT_TIME, p.TYPE "
           + "from ticket t,parking p where p.parking_number ="
-          + " t.parking_number and t.VEHICLE_REG_NUMBER=? order by t.IN_TIME  limit 1";
+          + " t.parking_number and t.VEHICLE_REG_NUMBER=? and t.OUT_TIME is null"
+          + " order by t.IN_TIME  limit 1";
 
   public static final String VERIFY_RECURRING_USER =
-      "select count(*) from ticket where VEHICLE_REG_NUMBER =?";
+      "select count(*) from ticket where VEHICLE_REG_NUMBER =? and OUT_TIME is not null";
 }
