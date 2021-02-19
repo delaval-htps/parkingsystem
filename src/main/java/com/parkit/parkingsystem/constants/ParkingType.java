d7399f041b0e344a56855c4f6d24e7a391fda03f
delaval <delaval.htps@gmail.com>
2021-02-19 20:54:19 +0100

    deletion of javadoc in /doc
    javadoc is now in target/site

--- src/main/java/com/parkit/parkingsystem/constants/ParkingType.java
+++ src/main/java/com/parkit/parkingsystem/constants/ParkingType.java
@@ -1,6 +1,12 @@
 package com.parkit.parkingsystem.constants;
 
+/**
+ * Enumeration to describe the type of vehicle for the park. It can be a CAR or a BIKE or unknown
+ * vehicle used just for test
+ *
+ * @author delaval
+ *
+ */
 public enum ParkingType {
-  CAR,
-  BIKE, UNKNOWN
+  CAR, BIKE, UNKNOWN
 }
